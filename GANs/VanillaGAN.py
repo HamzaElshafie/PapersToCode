6cb9d8502fa66d7dc6bc0586c89c9842a2756806
HamzaElshafie <hamzaelshafie@outlook>
2025-01-26 21:19:51 +0000

    Added skeleton for code and create a jupyter book.

--- GANs/VanillaGAN.py
+++ GANs/VanillaGAN.py
@@ -1,5 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import numpy as np
 
+
+class Generator(nn.module):
+    pass
+
+
+class Discriminator(nn.module):
+    pass
 
